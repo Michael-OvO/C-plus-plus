076d71279d4a02730017a32d623a9c02906bd035
Michael.Wang <85395001+Michael-OvO@users.noreply.github.com>
2022-08-29 11:57:14 +0800

    upd

---
+++ code/3-variables.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int tuna = 6;
+    cout<<tuna<<endl;
+    return 0;
+}
---
+++ code/5-If_statement.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+// if statement
+int main()
+{
+
+    // Relational Operators
+    // True test
+    if (3 < 6)
+    {
+        cout << "test is correct" << endl;
+    }
+    // false test
+    if (3 > 6)
+    {
+        // the condition is false, and will not be printed
+        cout << "test is false" << endl;
+    }
+    // quality operators
+    if (3 == 3)
+    {
+        cout << "quality operator test!" << endl;
+    }
+    if (6 != 3)
+    {
+        cout << "6!=3!" << endl;
+    }
+}
